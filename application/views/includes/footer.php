08cd98c7348938de0aafcbe0527c74f05287464c
vishnu <vishnu.kvs@gmail.com>
2013-03-26 22:39:20 +1100

    added footer to the project

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,21 +1,25 @@
 
-    </div> <!-- /container -->
+      <hr>
+      <!-- footer-->
+      <footer id="footer">
+        <p>&copy;2013 | Art of Living Foundation </p>
+      </footer>
+  </div> <!-- /container -->
 
    <!-- Le javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     
-    <script src="./js/jquery-countdown.js"></script>
-    <script src="./js/bootstrap-carousel.js"></script>
-   
-    <script type="text/javascript">
+    <script src="<?=base_url('js/jquery-countdown.js');?>"></script>
+    <script src="<?=base_url('js/bootstrap-carousel.js');?>"></script>
+     <script type="text/javascript">
       $('#myCarousel').carousel({
       interval: 5000
     })
     </script>
 
 </script>
-<script src="./js/youtube-plugin.js"></script>
+<script src="<?=base_url('js/youtube-plugin.js');?>"></script>
    <script type="text/javascript">
         $(function () {
             $("a.youtube").YouTubePopup({ autoplay: 0 });
