5fc0b2c402eaf29ced85c921f3382236a01358b1
vishnu <vishnu.kvs@gmail.com>
2013-03-26 22:08:56 +1100

    changed config files

--- application/config/config.php
+++ application/config/config.php
@@ -39,7 +39,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= '';
+$config['base_url']	= 'http://localhost/newyogathon';
 
 /*
 |--------------------------------------------------------------------------
@@ -51,7 +51,7 @@ $config['base_url']	= '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
